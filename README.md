d4dc4167c8d842971f8dd41b135a8038ff719ee2
charb <charb@DESKTOP-MBMDRV0>
2017-04-09 02:46:15 +0200

    Définition des objectifs du projet et mise à jours du fichier .java

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # TestProject
 Projet de test de developpement JAVA
 
-Readme du projet de test de développement java : Essai 1 de commit
+- Objectifs du projet détaillé : 
+	1. Création du jeu du pendu en mode console
+	2. Mise à jour du jeu avec : 
+		- Utilisation d'un fichier pour générer le mot à trouver
+		- Ecrire dans un fichier les statistiques de la partie : 
+			- Nombre de tentatives
+			- Nombre de mots trouvés
+			- Temps de jeu 
+	3. Lier l'application à une base de données
+	4. Création d'un programme batch pour alimenter la base de données
--- src/Launch.java
+++ src/Launch.java
@@ -1,8 +1,49 @@
+/**
+ * Classe de lancement du projet
+ * @author Loïc
+ *
+ */
 
 public class Launch {
 
-	public static void main (String args[]){
-		System.out.println("essai de projet");
-	}
+	// Attributs
+	private boolean playing = false; // Initilalisé à false de base pour lancer le jeu	
+	private String rules = "Dans ce jeu du pendu l'objectif est de trouvé le mot sélectionné par l'ordinateur."
+			+ "\nLe nombre d'essai est illimité."
+			+ "\n\tBonne chance !"; // Règle du jeu
 	
+	private String[] tabWords = new String[]{"Test","Tete","Ordinateur","Rien"};
+
+	/**
+	 * Méthode princpale de lancement du projet
+	 * @param args
+	 */
+	public static void main (String args[]){
+		/**
+		 * On initialise un objet "Launch" pour éviter de créer des attributs static
+		 * Pas obligatoire
+		 */
+		Launch launch = new Launch();
+		
+		System.out.println("Le jeu du pendu V.1");
+		System.out.println(launch.rules); // On affiche les règles du jeu
+		
+		// TODO : 1 - Sélectionner un mot dans un tableau de mot prédéfini dans les attributs
+		
+		// TODO : 2 - Initialisation d'un tableau contenant le nombre de caractère présent
+		// dans le mot avec les vraies lettre pour le première et dernière poisition et "-" pour le reste
+		// On affiche le mot en même temps
+			// TODO 3 : On affiche le mot à trouver
+
+		while(launch.playing == false){ // /!\ Attention à la boucle infini /!\
+			// TODO : 4 - On demande à l'utilisateur de saisir une lettre
+			
+			// TODO : 5 - On vérifie si la lettre est présente ou non
+				// TODO : 6 - Si elle est présente on l'ajoute à son emplacement dans le tableau de caractère
+
+				// TODO : 7 - On vérifie si oui ou non le mot est trouvé
+					// TODO : 8 - Si le mot est trouvé on affiche un message de fin + arrête
+				// TODO : 9 - Si mot pas trouvé on continue
+		}
+	}
 }
